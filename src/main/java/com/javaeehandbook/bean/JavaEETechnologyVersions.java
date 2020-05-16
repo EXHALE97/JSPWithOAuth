b1e46e7a5f11e2426f2def876790755ed6ac26dd
Yulya Evgrafova <yulyaevrafova92@gmail.com>
2020-05-16 15:19:36 +0300

    Added JSP and OAuth

---
+++ src/main/java/com/javaeehandbook/bean/JavaEETechnologyVersions.java
@@ -0,0 +1,52 @@
+package com.javaeehandbook.bean;
+
+class JavaEETechnologyVersions {
+    private String versionForJava4 = "";
+    private String versionForJava5 = "";
+    private String versionForJava6 = "";
+    private String versionForJava7 = "";
+    private String versionForJava8 = "";
+
+    JavaEETechnologyVersions() {
+    }
+
+    String getVersionForJava4() {
+        return versionForJava4;
+    }
+
+    void setVersionForJava4(String rVForJava4) {
+        this.versionForJava4 = rVForJava4;
+    }
+
+    String getVersionForJava5() {
+        return versionForJava5;
+    }
+
+    void setVersionForJava5(String rVForJava5) {
+        this.versionForJava5 = rVForJava5;
+    }
+
+    String getVersionForJava6() {
+        return versionForJava6;
+    }
+
+    void setVersionForJava6(String rVForJava6) {
+        this.versionForJava6 = rVForJava6;
+    }
+
+    String getVersionForJava7() {
+        return versionForJava7;
+    }
+
+    void setVersionForJava7(String rVForJava7) {
+        this.versionForJava7 = rVForJava7;
+    }
+
+    String getVersionForJava8() {
+        return versionForJava8;
+    }
+
+    void setVersionForJava8(String rVForJava8) {
+        this.versionForJava8 = rVForJava8;
+    }
+}
---
+++ src/main/java/com/javaeehandbook/dao/TechnologyDao.java
@@ -0,0 +1,164 @@
+package com.javaeehandbook.dao;
+
+import com.javaeehandbook.bean.JavaEETechnology;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TechnologyDao {
+    private static final Logger log;
+    private static Database database;
+
+    static {
+        log = LogManager.getLogger(TechnologyDao.class);
+        try {
+            database = new Database();
+        } catch (Exception e) {
+            database = null;
+            log.error(e.getMessage());
+        }
+    }
+
+    public static List<JavaEETechnology> getAllTechnologies() throws SQLException {
+        String SQL;
+        ResultSet rs;
+        List<JavaEETechnology> list = new ArrayList<JavaEETechnology>();
+        SQL = "SELECT *\n" +
+                "FROM java_technologies INNER JOIN used_versions\n" +
+                "ON java_technologies.versions = used_versions.used_versions_id;";
+
+        rs = database.getConnection().createStatement().executeQuery(SQL);
+
+        while (rs.next()) {
+            list.add(fromResultSetToObject(rs));
+        }
+
+        return list;
+    }
+
+    public static int addTechnology(JavaEETechnology technology) throws SQLException {
+        String SQL;
+        PreparedStatement preparedStatement;
+
+        SQL = "INSERT INTO used_versions(java_4, java_5, java_6, java_7, java_8) \n" +
+                "VALUES(?, ?, ?, ?, ?)\n" +
+                "ON DUPLICATE KEY UPDATE java_4=?, java_5=?, java_6=?, java_7=?, java_8=?;";
+        preparedStatement = database.getConnection().prepareStatement(SQL);
+        preparedStatement.setString(1, technology.getVersionForJava4());
+        preparedStatement.setString(2, technology.getVersionForJava5());
+        preparedStatement.setString(3, technology.getVersionForJava6());
+        preparedStatement.setString(4, technology.getVersionForJava7());
+        preparedStatement.setString(5, technology.getVersionForJava8());
+        preparedStatement.setString(6, technology.getVersionForJava4());
+        preparedStatement.setString(7, technology.getVersionForJava5());
+        preparedStatement.setString(8, technology.getVersionForJava6());
+        preparedStatement.setString(9, technology.getVersionForJava7());
+        preparedStatement.setString(10, technology.getVersionForJava8());
+        int status = preparedStatement.executeUpdate();
+
+        if (status > 0) {
+            SQL = "INSERT INTO java_technologies(tech_name, versions, description)\n" +
+                    "VALUES(?,\n" +
+                    "(SELECT used_versions_id\n" +
+                    "FROM used_versions\n" +
+                    "WHERE java_4=? AND java_5=? AND java_6=? AND java_7=? AND java_8=?), ?)\n" +
+                    "ON DUPLICATE KEY UPDATE tech_name=?;";
+            preparedStatement = database.getConnection().prepareStatement(SQL);
+            preparedStatement.setString(1, technology.getName());
+            preparedStatement.setString(2, technology.getVersionForJava4());
+            preparedStatement.setString(3, technology.getVersionForJava5());
+            preparedStatement.setString(4, technology.getVersionForJava6());
+            preparedStatement.setString(5, technology.getVersionForJava7());
+            preparedStatement.setString(6, technology.getVersionForJava8());
+            preparedStatement.setString(7, technology.getDescription());
+            preparedStatement.setString(8, technology.getName());
+            status = preparedStatement.executeUpdate();
+        }
+
+        return status;
+    }
+
+    public static void deleteTechnology(JavaEETechnology technology) throws SQLException {
+        String SQL;
+        PreparedStatement preparedStatement;
+        SQL = "DELETE FROM java_technologies\n" +
+                "WHERE tech_id=?;";
+        preparedStatement = database.getConnection().prepareStatement(SQL);
+        preparedStatement.setInt(1, technology.getId());
+        preparedStatement.executeUpdate();
+    }
+
+    public static void updateTechnology(JavaEETechnology technology) throws SQLException {
+        String SQL;
+        PreparedStatement preparedStatement;
+
+        SQL = "INSERT INTO used_versions(java_4, java_5, java_6, java_7, java_8) \n" +
+                "VALUES(?, ?, ?, ?, ?)\n" +
+                "ON DUPLICATE KEY UPDATE java_4=?, java_5=?, java_6=?, java_7=?, java_8=?;";
+        preparedStatement = database.getConnection().prepareStatement(SQL);
+        preparedStatement.setString(1, technology.getVersionForJava4());
+        preparedStatement.setString(2, technology.getVersionForJava5());
+        preparedStatement.setString(3, technology.getVersionForJava6());
+        preparedStatement.setString(4, technology.getVersionForJava7());
+        preparedStatement.setString(5, technology.getVersionForJava8());
+        preparedStatement.setString(6, technology.getVersionForJava4());
+        preparedStatement.setString(7, technology.getVersionForJava5());
+        preparedStatement.setString(8, technology.getVersionForJava6());
+        preparedStatement.setString(9, technology.getVersionForJava7());
+        preparedStatement.setString(10, technology.getVersionForJava8());
+        preparedStatement.executeUpdate();
+
+        SQL = "UPDATE java_technologies\n" +
+                "SET tech_name=?, versions=(SELECT used_versions_id\n" +
+                "FROM used_versions\n" +
+                "WHERE java_4=? AND java_5=? AND java_6=? AND java_7=? AND java_8=?),\n" +
+                "description=?\n" +
+                "WHERE tech_id=?;";
+        preparedStatement = database.getConnection().prepareStatement(SQL);
+        preparedStatement.setString(1, technology.getName());
+        preparedStatement.setString(2, technology.getVersionForJava4());
+        preparedStatement.setString(3, technology.getVersionForJava5());
+        preparedStatement.setString(4, technology.getVersionForJava6());
+        preparedStatement.setString(5, technology.getVersionForJava7());
+        preparedStatement.setString(6, technology.getVersionForJava8());
+        preparedStatement.setString(7, technology.getDescription());
+        preparedStatement.setInt(8, technology.getId());
+        preparedStatement.executeUpdate();
+    }
+
+    public static JavaEETechnology getTechnologyById(Integer id) throws SQLException {
+        String SQL;
+        ResultSet rs;
+
+        SQL = "SELECT *\n" +
+                "FROM java_technologies INNER JOIN used_versions\n" +
+                "ON java_technologies.versions = used_versions.used_versions_id\n" +
+                "WHERE java_technologies.tech_id = ?;";
+
+        PreparedStatement statement = database.getConnection().prepareStatement(SQL);
+        statement.setInt(1, id);
+        rs = statement.executeQuery();
+        rs.next();
+
+        return fromResultSetToObject(rs);
+    }
+
+    private static JavaEETechnology fromResultSetToObject(ResultSet rs) throws SQLException {
+        JavaEETechnology technology = new JavaEETechnology();
+        technology.setId(rs.getInt("tech_id"));
+        technology.setName(rs.getString("tech_name"));
+        technology.setVersionForJava4(rs.getString("java_4"));
+        technology.setVersionForJava5(rs.getString("java_5"));
+        technology.setVersionForJava6(rs.getString("java_6"));
+        technology.setVersionForJava7(rs.getString("java_7"));
+        technology.setVersionForJava8(rs.getString("java_8"));
+        technology.setDescription(rs.getString("description"));
+
+        return technology;
+    }
+}
